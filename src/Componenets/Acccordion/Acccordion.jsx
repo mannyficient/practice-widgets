7f0da966a289af9c232a264d27d4b34cd083a994
mannyficient <smilley197@gmail.com>
2021-08-01 05:21:17 -0400

    finished first widget (the accordion) with functional component

--- src/Componenets/Acccordion/Acccordion.jsx
+++ src/Componenets/Acccordion/Acccordion.jsx
@@ -1,29 +1,34 @@
 import React, { useState } from "react";
 
 const Acccordion = ({ items }) => {
+  // initializing state
   const [activeIndex, setActiveIndex] = useState(null);
+
+  //setting state
   const onTitleClick = (index) => {
     setActiveIndex(index);
   };
+
+  // generate jsx
   const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? "active" : "";
     return (
       <React.Fragment key={item.title}>
-        <div className='title active' onClicl={() => onTitleClick(index)}>
+        <div
+          className={`title ${active}`}
+          active
+          onClick={() => onTitleClick(index)}
+        >
           <i className='dropdown icon'></i>
           {item.title}
         </div>
-        <div className='conten active'>
+        <div className={`content ${active}`}>
           <p>{item.content}</p>
         </div>
       </React.Fragment>
     );
   });
-  return (
-    <div className='ui styled accordion'>
-      {renderedItems}
-      <h1>{activeIndex}</h1>
-    </div>
-  );
+  return <div className='ui styled accordion'>{renderedItems}</div>;
 };
 
 export default Acccordion;
