e97b5ceb72556015c6777555446d85d7d22df000
mannyficient <smilley197@gmail.com>
2021-08-01 11:17:32 -0400

    finished drop down menu implementing event bubbling

--- src/Componenets/Dropdown/Dropdown.jsx
+++ src/Componenets/Dropdown/Dropdown.jsx
@@ -1,7 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    document.body.addEventListener(
+      "click",
+      (event) => {
+        if (ref.current.contains(event.target)) {
+          return;
+        }
+        setOpen(false);
+      },
+      { capture: true }
+    );
+  }, []);
 
   const renderedOptions = options.map((option) => {
     if (option.value === selected.value) {
@@ -19,7 +33,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
   });
 
   return (
-    <div className='ui form'>
+    <div ref={ref} className='ui form'>
       <div className='field'>
         <div className='label'>Select a Color</div>
         <div
@@ -32,6 +46,12 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
             {renderedOptions}
           </div>
         </div>
+      </div>
+      <div
+        className='dummy'
+        style={{ color: `${selected.value}`, fontSize: "20px" }}
+      >
+        Hi, I am Emmanuel
       </div>
     </div>
   );
