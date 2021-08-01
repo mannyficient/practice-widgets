9aedd090cce55a4643cea42f8e7411b14db21678
mannyficient <smilley197@gmail.com>
2021-08-01 07:02:13 -0400

    finished building penultimate version of the search widget

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
 import "./App.css";
-import Acccordion from "./Componenets/Acccordion/Acccordion";
+// import Acccordion from "./Componenets/Acccordion/Acccordion";
+import Search from "./Componenets/Search/Search.jsx";
 
-const items = [
-  {
-    title: "What is react?",
-    content: "React is a front end javascript framework",
-  },
-  {
-    title: "Why use React?",
-    content: "React is a favorite JS library amongst developers",
-  },
-  {
-    title: "How do you use React?",
-    content: "You use react by creating components",
-  },
-];
+// const items = [
+//   {
+//     title: "What is react?",
+//     content: "React is a front end javascript framework",
+//   },
+//   {
+//     title: "Why use React?",
+//     content: "React is a favorite JS library amongst developers",
+//   },
+//   {
+//     title: "How do you use React?",
+//     content: "You use react by creating components",
+//   },
+// ];
 function App() {
   return (
     <div className='App'>
-      <Acccordion items={items}></Acccordion>
+      {/* <Acccordion items={items}></Acccordion> */}
+      <Search></Search>
     </div>
   );
 }
